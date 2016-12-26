58c53aea5b418a29f03befe914473c0112a444df
blackleg <hectorespertpardo@gmail.com>
2016-12-26 14:02:25 +0100

    Add travis before-script

--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+source .travis/utils.sh
+
+if isDeployable; then
 	openssl aes-256-cbc -K $encrypted_65fd749ede06_key -iv $encrypted_65fd749ede06_iv -in codesigning.asc.enc -out codesigning.asc -d
 	gpg --fast-import codesigning.asc
 fi
