1f21c57ce3850027db58505484445f970734a635
blackleg <hectorespertpardo@gmail.com>
2016-12-26 14:18:52 +0100

    Changes in before script

--- .travis/before-script.sh
+++ .travis/before-script.sh
@@ -2,6 +2,6 @@
 source .travis/utils.sh
 
 if isDeployable; then
-	openssl aes-256-cbc -K $encrypted_65fd749ede06_key -iv $encrypted_65fd749ede06_iv -in codesigning.asc.enc -out codesigning.asc -d
-	gpg --fast-import codesigning.asc
+	openssl aes-256-cbc -K $encrypted_65fd749ede06_key -iv $encrypted_65fd749ede06_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+	gpg --fast-import .travis/codesigning.asc
 fi
